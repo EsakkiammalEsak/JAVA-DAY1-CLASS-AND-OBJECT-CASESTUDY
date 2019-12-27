748ab74dfd6662f5af580e18677f8f5d9a2d087a
kowsisk <kowsisk@LAPTOP-N3IT2TON.mshome.net>
2019-12-27 12:27:36 +0530

    class and object

---
+++ src/ClassandObject/Addition.java
@@ -0,0 +1,33 @@
+package ClassandObject;
+
+public class Addition {
+
+	//public static void main(String[] args) {
+		private int a;
+		private int b;
+		//private int answer;
+		/*Addition(int x,int y)
+		{
+		this.a=x;
+		this.b=y;
+		//this.answer=a+b;
+		}*/
+		Addition()
+		{
+		}
+		
+		public Addition(int a, int b) {
+			super();
+			this.a = a;
+			this.b = b;
+			System.out.println(a+b);
+		}
+	/*	public void display()
+		{
+			System.out.println(answer);
+		}*/
+		
+
+	//}
+
+}
---
+++ src/ClassandObject/jfs.java
@@ -0,0 +1,43 @@
+package ClassandObject;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class jfs {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jfs window = new jfs();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public jfs() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
